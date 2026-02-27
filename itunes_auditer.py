b75e2ec50b1b94c298413a3a9da9a62694c463fb
gybeal <gywbeal@gmail.com>
2026-02-26 20:37:04 -0600

    add basic depth first search with ugly print and filecounts

--- itunes_auditer.py
+++ itunes_auditer.py
@@ -12,3 +12,41 @@ The basics:
     I have to touch any folder with a potentially missing track anyway, and these should only make up a small fraction.
 
     '''
+
+# MAJOR TODO
+# evaluate path.walk() as an alternative to the relatively naive DFS here
+
+from pathlib import Path
+
+YOUR_BASE_PATH_HERE = Path("basepath.txt").read_text().strip()
+# TODO: very quick and dirty anonymization. put in function later.
+#TODO: raw strings best practice y/n?
+
+
+#################
+# FUNCTION LAND #
+#################
+
+# TODO refactor this to a helper class when it is not just duct tape.
+
+def dfs_folder_traverse(folder):
+    child_folders = [f for f in folder.iterdir() if f.is_dir()]
+
+    for f in child_folders:
+        print(f'down one level to {f.name}')
+        dfs_folder_traverse(f)
+    else:
+        numfiles = len([file.name for file in folder.iterdir() if file.is_file()])
+        print(f"{numfiles} files in {folder.name} here...")
+        return
+
+
+
+############
+# mainland #
+############
+
+base = Path(YOUR_BASE_PATH_HERE)
+
+
+dfs_folder_traverse(base)
